12c0ee8b3d3e1c8d2092b23212e37ed5278cebc8
nicolasgonzalez98 <nicolasgonzalez470@gmail.com>
2022-08-11 19:18:35 -0300

    'lenguajes'

--- 14_Day_Higher_order_functions/level3.py
+++ 14_Day_Higher_order_functions/level3.py
@@ -3,9 +3,29 @@ from countries_data import paises as countries
 def by_name(e):
     return e['capital'] 
 
-name = sorted(countries, key=by_name, reverse=False)
+# name = sorted(countries, key=by_name, reverse=False)
 
-capital = sorted(countries, key=lambda x:x['capital'])
+# capital = sorted(countries, key=lambda x:x['capital'])
 
-population = sorted(countries, key=lambda x : x['population'])
+# population = sorted(countries, key=lambda x : x['population'])
+
+##Sort out the ten most spoken languages by location.
+
+languages = {}
+
+for pais in countries:
+    for language in pais['languages']:
+        if language in languages:
+            languages[language]+=1
+        else:
+            languages[language]=1
+
+total = sorted(languages.items())
+
+total = sorted(total, key= lambda x : x[1], reverse=True)
+
+espaniol = list(i['name'] for i in countries if 'Spanish' in i['languages'])
+
+print(espaniol)
+
 
